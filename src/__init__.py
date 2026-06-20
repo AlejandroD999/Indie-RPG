c97ab0982b95b2b5abb3213da7904cefc8b3e654
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-06-20 15:53:27 -0500

    Moved paths to separate file __init__ -> paths

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SRC_PATH = os.path.dirname(__file__)
-BG_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "statics/themes/nature_1/origbig.png")
-
---
+++ src/paths.py
@@ -0,0 +1,5 @@
+import os
+
+SRC_PATH = os.path.dirname(__file__)
+BG_IMAGE_PATH = os.path.join(SRC_PATH, "statics/themes/nature_1/origbig.png")
+BUG_STATICS = os.path.join(SRC_PATH, "statics/bugs")
