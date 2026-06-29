8af6722fa0a4f9116ccfbcd12a86475f8ad1daf3
AlejandroD <alejandro.gonzalez.dev8@gmail.com>
2026-06-29 08:46:26 -0500

    Created dummy player

--- src/characters/player.py
+++ src/characters/player.py
@@ -1,4 +1,4 @@
-from ..paths import BUG_STATICS
+from ..paths import BUG_STATICS, PLAYER_STATICS
 from ..config import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
 import os
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.__SCREEN_RECT = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
         self.sprites = []
-        self.sprites.append(pygame.image.load(os.path.join(BUG_STATICS, "bug-1.png")).convert_alpha())
+        self.sprites.append(pygame.image.load(os.path.join(PLAYER_STATICS, "stick_man_0.png")).convert_alpha())
         self.current_sprite = 0
 
         self.image = self.sprites[self.current_sprite]
--- src/paths.py
+++ src/paths.py
@@ -3,3 +3,4 @@ import os
 SRC_PATH = os.path.dirname(__file__)
 BG_IMAGE_PATH = os.path.join(SRC_PATH, "statics/themes/nature_1/origbig.png")
 BUG_STATICS = os.path.join(SRC_PATH, "statics/bugs")
+PLAYER_STATICS = os.path.join(SRC_PATH, "statics/player")
