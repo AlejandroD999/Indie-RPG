155d3e2a0f750abd4eb4c2225770b9eb47b980dc
AlejandroD <alejandro.gonzalez.dev8@gmail.com>
2026-06-30 13:47:22 -0500

    Defined MainMenu object

--- src/UI/menu.py
+++ src/UI/menu.py
@@ -1,7 +1,28 @@
 import pygame
+import sys
 
-def main_menu():
-    running = True
+class MainMenu():
+    def __init__(self):
+        self.running = True
+    
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
-    while running:
+    def update(self):
         pass
+
+    def draw(self):
+        pass
+
+    def run(self):
+        while self.running:
+
+            self.handle_events()
+            self.update()
+            self.draw()
+
+
+        pygame.quit
+        sys.exit
