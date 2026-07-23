05b54450ffc28395789135977dedb41ae3d6d10b
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-07-22 19:29:28 -0500

    Defined inventory class skeleton

---
+++ src/UI/inventory.py
@@ -0,0 +1,39 @@
+
+'''
+Idea so far: Make inventory a new screen.
+
+1. Key pressed while in game: Opens inventory
+jjj
+2. Current screen (in app) is inventory. 
+    - Inventory drawn and interacted to the right side of the window
+
+3. Player does actions
+    If action == QUIT KEY or ESC:
+       switch screen to game 
+
+'''
+class Inventory:
+    def __init__(self, width, height):
+        pass
+    
+    def insert_item(self, item):
+        pass
+
+    def remove_item(self, item):
+        pass
+
+    def return_items(self):
+        # Return in form of array or dict
+        pass
+    
+    def save_inventory(self):
+        # Connect to db and dump
+        pass
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+
